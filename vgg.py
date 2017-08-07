001c47b49e9dc89cdd1614095637380a4da3b93c
Nathan LaPierre <nathanl2012@gmail.com>
2017-08-07 15:46:41 -0700

    added scrip for pileups of whole read length and updated CNN code

--- vgg.py
+++ vgg.py
@@ -15,12 +15,14 @@ from __future__ import absolute_import
 import argparse, glob, sys
 import numpy as np
 from scipy import ndimage
+from scipy.stats import pearsonr, spearmanr
 
 import warnings
 
 import keras
 from keras.models import Model, Sequential
 from keras.layers import Conv2D, Dense, Dropout, Flatten, GlobalAveragePooling2D, GlobalMaxPooling2D, Input, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
 from keras.engine.topology import get_source_inputs
 from keras.utils import layer_utils
 from keras.utils.data_utils import get_file
@@ -69,27 +71,32 @@ def VGG16(include_top=True, weights='imagenet',
         else:
             img_input = input_tensor
 
+    x = BatchNormalization()(img_input)
     # Block 1
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(x)#(img_input)
     x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+    #x = BatchNormalization()(x)
 
     # Block 2
     x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
     x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+    #x = BatchNormalization()(x)
 
     # Block 3
     x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
     x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
     x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+    #x = BatchNormalization()(x)
 
     # Block 4
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+    #x = BatchNormalization()(x)
 
     # Block 5
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
@@ -155,17 +162,25 @@ def VGG16(include_top=True, weights='imagenet',
     # model.layers.pop()
     #'''
     #x = Flatten(name='flatten')(model.layers[-1].output)
-    x = Dense(4096, activation='softmax', name='fc1')(model.layers[-1].output)#(x)
+    #x = BatchNormalization()(x)
+    x = Dense(4096, activation='relu', name='fc1')(model.layers[-1].output)#(x)
     x = Dropout(0.5)(x)
-    x = Dense(4096, activation='tanh', name='fc2')(x)
+    #x = BatchNormalization()(x)
+    x = Dense(4096, activation='relu', name='fc2')(x)
     x = Dropout(0.5)(x)
+    for i in range(0):
+        #x = BatchNormalization()(x)
+        x = Dense(4096, activation='relu', name='fc'+str(i+3))(x)
+        x = Dropout(0.5)(x)	
     #'''
-    x = Dense(1, activation='tanh', name='predictions_new')(x)
+    #x = BatchNormalization()(x)
+    x = Dense(1, activation='relu', name='predictions_new')(x)
     new_model = Model(inputs, x, name='vgg16_new')
     return new_model
 
 
 def eval_preds(actual, predicted):
+	predicted = np.array([i[0] for i in predicted])
 	print 'Actual:'
 	print actual
 	print '\nPredicted:'
@@ -183,11 +198,14 @@ def eval_preds(actual, predicted):
 			within5 += percented
 		if errors[i] < 0.1:
 			within10 += percented
-	outside10 = 100.0 - within1 - within5 - within10
+	print
 	print str(within1) + ' percent of predictions within 1.0 of actual'
 	print str(within5) + ' percent of predictions within 5.0 of actual'
 	print str(within10) + ' percent of predictions within 10.0 of actual'		
-	print str(outside10) + ' percent of predictions outside 10.0 from actual'
+	print str(100.0 - within10) + ' percent of predictions outside 10.0 from actual'
+	print
+	print 'Pearson correlation: ' + str(pearsonr(actual, predicted)[0])
+	print 'Spearman rank correlation: ' + str(spearmanr(actual, predicted)[0])
 
 
 def main():
@@ -207,20 +225,22 @@ def main():
 		labels_dict[splits[0]] = [float(i)/100.0 for i in splits[1].split(',')]
 	labels_file.close()
 
-	data, labels = [], []
+	data, labels, endpoints = [], [], []  # endpoints is position where full reads end
 	for fname in glob.glob(args.input+'*.png'):
 		imname = fname.split('/')[-1][:-4]
 		if imname not in labels_dict:
 			continue
 		imlabels = labels_dict[imname]
-		zero_segments = 0
-		for i in imlabels:
+		zero_segments, pos = [0,0], 0
+		for i in imlabels:  # here we determine the 0 identity segments on the end of reads, which are junk
 			if i == 0:
-				zero_segments += 1
+				zero_segments[pos] += 1
 			else:
-				break
-		imarray = ndimage.imread(fname)
-		for i in range(zero_segments, len(imlabels)-2):
+				pos=1
+		imarray = ndimage.imread(fname, mode='RGB')
+
+		# break read into windows, excluding junk 0s at the ends
+		for i in range(zero_segments[0], len(imlabels)-2-zero_segments[1]):
 			sidelen = args.segment_size / 2  # extra space on each side of segment in window
 			if (i+2)*args.segment_size+sidelen > len(imarray[0]):
 				break
@@ -228,28 +248,45 @@ def main():
 			label = imlabels[i+1]
 			data.append(window)
 			labels.append(label)
-	data, labels = data[:64], labels[:64]  # debug mode
+		endpoints.append(len(data))
+
+	data, labels = data[:640], labels[:640]  # debug mode
 	data = np.array(data)
 	labels = np.array(labels)
-	train_index, valid_index = int(0.6*len(data)), int(0.8*len(data))
 
+	# we set the indices to split the data into train/validation/test
+	# and round these up to the end of the nearest read to prevent overfitting
+	train_index, valid_index = int(0.6*len(data)), int(0.8*len(data))
+	for point in endpoints:
+		if point >= train_index:
+			train_index = point
+			break
+	for point in endpoints:
+		if point >= valid_index:
+			valid_index = point
+			break
+
+	'''
+	# we center the data at 0 by computing the average pixel and subtracting it
 	avg = [0.0, 0.0, 0.0]
 	for image in data:
 		for row in image:
 			for pixel in row:
+				if all(p == 0.0 for p in pixel):  # ignore all 0 (black) pixels
+					continue
 				avg = [avg[i]+pixel[i] for i in range(len(pixel))]
 	numpixels = float(data.shape[0] * data.shape[1] * data.shape[2])
 	avg = [avg[i]/numpixels for i in range(len(avg))]
-
 	for image in data:
 		for row in image:
 			for pixel in row:
 				pixel = [pixel[i]-avg[i] for i in range(len(pixel))]
+	'''
 
 	#vgg = VGG16(include_top=False, weights='imagenet', input_tensor=None, input_shape=data[0].shape, pooling='max')
 	vgg = VGG16(include_top=False, weights=None, input_tensor=None, input_shape=data[0].shape, pooling='max')
-	vgg.compile(optimizer='adadelta', loss='mean_squared_error')
-	vgg.fit(data[:train_index], labels[:train_index], epochs=50, validation_data=(data[train_index:valid_index], labels[train_index:valid_index]), batch_size=64)
+	vgg.compile(optimizer='sgd', loss='mean_squared_error')
+	vgg.fit(data[:train_index], labels[:train_index], epochs=5, validation_data=(data[train_index:valid_index], labels[train_index:valid_index]), batch_size=64)
 	print 'Predicting...'
 	predictions = vgg.predict(data[valid_index:], batch_size=64)
 	eval_preds(labels[valid_index:], predictions)
